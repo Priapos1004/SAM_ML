a05a708320c8e89de9964286f8357c228b22aa0c
Samuel Brinkmann <samuel@brinkmann-familie.de>
2022-04-29 15:50:25 +0200

    added count and tfidf vectorizer to embeddings script
    - changed error message if sentence-transformers is not installed

--- sam_ml/data/__init__.py
+++ sam_ml/data/__init__.py
@@ -1,5 +1,7 @@
+from .embeddings import build_embeddings
 from .synthetic_data import synt_data
 
 __all__ = {
     "Synthesis data function": "synt_data",
+    "Build embeddings for text": "build_embeddings",
 }
