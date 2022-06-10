29d5cc48d7c726d5663584d9e7d5002a3c05882a
Samuel Brinkmann <samuel@brinkmann-familie.de>
2022-06-10 12:58:15 +0200

    updated version to 0.1.4 on dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="sam_ml",
-    version="0.1.3",
+    version="0.1.4",
     description="a library for ML programing created by Samuel Brinkmann",
     packages=find_packages(),
     package_data={},
