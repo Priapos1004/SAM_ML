e6c83e3c9867ed618f0497d262eb66d19cf8e742
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2023-07-07 10:05:11 +0200

    fixed Ctest pytest

--- test/test_CTest.py
+++ test/test_CTest.py
@@ -43,16 +43,16 @@ def test_find_best_model_mass_search_error():
         ctest = CTest()
         ctest.find_best_model_mass_search(x_train, y_train, x_test, y_test)
 
-X, Y = make_classification(n_samples = 2000,
+X_new, Y_new = make_classification(n_samples = 2000,
                             n_features = 5,
                             n_informative = 5,
                             n_redundant = 0,
                             n_classes = 3,
                             weights = [.2, .3, .8])
-X = pd.DataFrame(X, columns=["col1", "col2", "col3", "col4", "col5"])
-Y = pd.Series(Y)
-x_train, x_test, y_train, y_test = train_test_split(X,Y, train_size=0.80, random_state=42)
+X_new = pd.DataFrame(X_new, columns=["col1", "col2", "col3", "col4", "col5"])
+Y_new = pd.Series(Y_new)
+x_train_new, x_test_new, y_train_new, y_test_new = train_test_split(X_new,Y_new, train_size=0.80, random_state=42)
 
 def test_find_best_model_mass_search():
         ctest = CTest()
-        ctest.find_best_model_mass_search(x_train, y_train, x_test, y_test, n_trails=3)
+        ctest.find_best_model_mass_search(x_train_new, y_train_new, x_test_new, y_test_new, n_trails=3)
