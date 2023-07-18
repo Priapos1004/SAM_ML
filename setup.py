0742f770a16b591468e935b7e68be134a7ff9176
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2023-07-18 11:36:44 +0200

    added package data
    - added pytest for get_plz_mapping

--- setup.py
+++ setup.py
@@ -7,16 +7,25 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="sam_ml-py",
-    version="0.11.0rc1",
+    version="0.11.0rc2",
     description="a library for ML programing created by Samuel Brinkmann",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.10",
     packages=find_packages(),
-    package_data={},
+    package_data={
+        "sam_ml": [
+            "data/regio/*.csv",
+            "data/regio/*.dbf",
+            "data/regio/*.prj",
+            "data/regio/*.shp",
+            "data/regio/*.shx",
+            "models/microwave_finish_sound.mp3",
+        ]
+    },
     scripts=[],
     install_requires=[
-        "scikit-learn<1.3", # version 1.3 has some issues currently (04/07/2023)
+        "scikit-learn<1.3",  # version 1.3 has some issues currently (04/07/2023)
         "pandas",
         "matplotlib",
         "numpy",
@@ -27,11 +36,13 @@ setup(
         "statsmodels",
         "sentence-transformers",
         "xgboost",
-        "ConfigSpace", # for hyperparameter tuning spaces
-        "geopandas", # for regio data (e.g. germany map plot)
+        "ConfigSpace",  # for hyperparameter tuning spaces
+        "geopandas",  # for regio data (e.g. germany map plot)
     ],
-    extras_require={"test": ["pytest", "pylint", "isort", "refurb", "black"],
-                    "with_swig": ["smac"]},
+    extras_require={
+        "test": ["pytest", "pylint", "isort", "refurb", "black"],
+        "with_swig": ["smac"],
+    },
     author="Samuel Brinkmann",
     license="MIT",
     tests_require=["pytest"],
