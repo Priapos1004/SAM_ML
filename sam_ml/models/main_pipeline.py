95e10c446061aab654553c745e9405985d5b9dc8
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2023-07-21 19:25:01 +0200

    fixed main_pipeline.py

--- sam_ml/models/main_pipeline.py
+++ sam_ml/models/main_pipeline.py
@@ -45,12 +45,6 @@ class Pipeline(Classifier):
                 elif not attribute_name.startswith("__"):
                     self.__dict__[attribute_name] = attribute_value
 
-            pipeline_methods = [method for method in dir(Pipeline) if callable(getattr(Pipeline, method)) and not method.startswith("__")]
-
-            # Inherit methods from the Pipeline class itself
-            for method_name in pipeline_methods:
-                setattr(self, method_name, getattr(Pipeline, method_name))
-
             self.model_name = model_name
             self.__class__.__name__ = model.__class__.__name__
             self.__classifier = model
