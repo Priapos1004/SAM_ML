49855062d5f5aa94253cefc89f7b8024cea05d81
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2023-07-21 18:46:58 +0200

    improved representation strings of data classes
    - created data parent class for data.preprocessing
    - removed simple_upsample from sampling.py

--- sam_ml/data/preprocessing/sampling.py
+++ sam_ml/data/preprocessing/sampling.py
@@ -7,56 +7,15 @@ from imblearn.under_sampling import (
     RandomUnderSampler,
     TomekLinks,
 )
-from sklearn.utils import resample
 
 from sam_ml.config import setup_logger
+
+from .main_data import DATA
 
 logger = setup_logger(__name__)
 
 
-def simple_upsample(x_train: pd.DataFrame, y_train: pd.Series, label: int | str = 1) -> tuple[pd.DataFrame, pd.DataFrame]:
-    """
-    function written by Sughu
-
-    @param:
-        x_train, y_train: trainings data for upsamling
-        label: label that shall be upsampled
-    
-    @return:
-        tuple x_train, y_train
-    """
-    count_per_sample = max(y_train.value_counts())
-    # Reset indexes
-    x_train.reset_index(drop=True)
-    y_train.reset_index(drop=True)
-    y_train = pd.DataFrame(y_train)
-    y_train.columns = ["y"]
-
-    # Stacking horizontally
-    df = pd.concat([x_train, y_train], axis=1)
-
-    df_1 = df[df["y"] == label]
-    # set other classes to another dataframe
-    other_df = df[df["y"] != label]
-    
-    # upsample the minority class
-    df_1_upsampled = resample(
-        df_1, random_state=42, n_samples=count_per_sample, replace=True
-    )
-    
-    # concatenate the upsampld dataframe
-    df_1_upsampled = df_1_upsampled.reset_index(drop=True)
-    other_df = other_df.reset_index(drop=True)
-
-    df_upsampled = pd.concat([df_1_upsampled, other_df])
-
-    # Split into X and y
-    x_train = df_upsampled.iloc[:, :-1]
-    y_train = df_upsampled.iloc[:, -1]
-
-    return x_train, y_train
-
-class Sampler:
+class Sampler(DATA):
     """ sample algorithm Wrapper class """
 
     def __init__(self, algorithm: str = "ros", random_state: int = 42, sampling_strategy="auto", **kwargs):
@@ -79,37 +38,26 @@ class Sampler:
             **kwargs:
                 additional parameters for sampler
         """
-        self.algorithm = algorithm
-        self._grid: dict[str, list] = {} # for pipeline structure
-
         if algorithm == "SMOTE":
-            self.sampler = SMOTE(random_state=random_state, sampling_strategy=sampling_strategy, **kwargs)
+            sampler = SMOTE(random_state=random_state, sampling_strategy=sampling_strategy, **kwargs)
         elif algorithm == "BSMOTE":
-            self.sampler = BorderlineSMOTE(random_state=random_state, sampling_strategy=sampling_strategy, **kwargs)
+            sampler = BorderlineSMOTE(random_state=random_state, sampling_strategy=sampling_strategy, **kwargs)
         elif algorithm == "rus":
-            self.sampler = RandomUnderSampler(random_state=random_state, sampling_strategy=sampling_strategy, **kwargs)
+            sampler = RandomUnderSampler(random_state=random_state, sampling_strategy=sampling_strategy, **kwargs)
         elif algorithm == "ros":
-            self.sampler = RandomOverSampler(random_state=random_state, sampling_strategy=sampling_strategy, **kwargs)
+            sampler = RandomOverSampler(random_state=random_state, sampling_strategy=sampling_strategy, **kwargs)
         elif algorithm == "tl":
-            self.sampler = TomekLinks(**kwargs)
+            sampler = TomekLinks(**kwargs)
         elif algorithm == "nm":
-            self.sampler = NearMiss(sampling_strategy=sampling_strategy, **kwargs)
+            sampler = NearMiss(sampling_strategy=sampling_strategy, **kwargs)
         elif algorithm == "cc":
-            self.sampler = ClusterCentroids(sampling_strategy=sampling_strategy, random_state=random_state, **kwargs)
+            sampler = ClusterCentroids(sampling_strategy=sampling_strategy, random_state=random_state, **kwargs)
         elif algorithm == "oss":
-            self.sampler = OneSidedSelection(random_state=random_state, **kwargs)
+            sampler = OneSidedSelection(random_state=random_state, **kwargs)
         else:
-            raise ValueError(f"type='{algorithm}' is not supported")
-
-    def __repr__(self) -> str:
-        sampler_params: str = ""
-        param_dict = self.get_params(False)
-        for key in param_dict:
-            if type(param_dict[key]) == str:
-                sampler_params += key+"='"+str(param_dict[key])+"', "
-            else:
-                sampler_params += key+"="+str(param_dict[key])+", "
-        return f"Sampler({sampler_params})"
+            raise ValueError(f"algorithm='{algorithm}' is not supported")
+        
+        super().__init__(algorithm, sampler)
 
     @staticmethod
     def params() -> dict:
@@ -120,13 +68,6 @@ class Sampler:
         param = {"algorithm": ["SMOTE", "BSMOTE", "rus", "ros", "tl", "nm", "cc", "oss"]}
         return param
 
-    def get_params(self, deep: bool = True):
-        return {"algorithm": self.algorithm} | self.sampler.get_params(deep)
-
-    def set_params(self, **params):
-        self.sampler.set_params(**params)
-        return self
-
     def sample(self, x_train: pd.DataFrame, y_train: pd.Series) -> tuple[pd.DataFrame, pd.Series]:
         """
         Function for up- and downsampling
@@ -134,6 +75,6 @@ class Sampler:
         @return:
             tuple x_train_sampled, y_train_sampled
         """
-        x_train_sampled, y_train_sampled = self.sampler.fit_resample(x_train, y_train)
+        x_train_sampled, y_train_sampled = self.transformer.fit_resample(x_train, y_train)
 
         return x_train_sampled, y_train_sampled
