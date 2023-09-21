1236781d2ac5bc498a632d458b1917df8917f556
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2023-09-22 00:41:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="sam_ml-py",
-    version="1.0.0",
+    version="1.1.0",
     description="a library for ML programing created by Samuel Brinkmann",
     long_description=long_description,
     long_description_content_type="text/markdown",
