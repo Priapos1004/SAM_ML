70b2b75bb6f764078658c209ee5bda5a4197817a
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2023-12-22 14:56:21 +0100

    generate uml diagram as svg
    - maybe solution to not use plantuml

---
+++ docs/diagram_generator.py
@@ -0,0 +1,82 @@
+import os
+from pathlib import Path
+
+from plantuml import PlantUML
+
+# PlantUML server URL or local JAR file path
+plantuml_server_url = 'http://www.plantuml.com/plantuml/svg/'
+
+# Initialize PlantUML object with server URL
+plantuml = PlantUML(url=plantuml_server_url)
+
+
+def generate_uml_diagram():
+    # PlantUML diagram code
+    uml_code = """
+    @startuml
+        skinparam classAttributeIconSize 0
+        hide empty members
+
+        package "sam_ml" {
+
+            package "sam_ml.data" {
+                abstract class Data
+                package "sam_ml.data.preprocessing" {
+                    circle " "
+                }
+                package "sam_ml.data.regio" {
+                    circle "     "
+                }
+            }
+
+            package "sam_ml.models" {
+                abstract class Model
+                abstract class Classifier
+                abstract class Regressor
+                abstract class BasePipeline
+                protocol create_pipeline
+                class DynamicPipeline
+                package "sam_ml.models.classifier" {
+                    circle "  "
+                }
+                package "sam_ml.models.regressor" {
+                    circle "   "
+                }
+
+                abstract class AutoML
+                package "sam_ml.models.automl" {
+                    circle "    "
+                }
+            }
+        }
+
+        AutoML <|-- "    "
+        Data <|-- " "
+        Model <|-- Classifier
+        Model <|-- Regressor
+        Model <|-- BasePipeline
+        Classifier <|-- "  "
+        Classifier <|-- DynamicPipeline
+        Regressor <|-- DynamicPipeline
+        Regressor <|-- "   "
+        DynamicPipeline <-- create_pipeline
+        BasePipeline <|-- DynamicPipeline
+
+    @enduml
+    """
+
+    # If the folder does not exist, create it
+    if not Path("docs/_images/").exists():
+        Path("docs/_images/").mkdir(parents=True)
+
+    # Generate the diagram and save it as an SVG file
+    svg_output = plantuml.processes(uml_code)
+    if svg_output:
+        Path("docs/_images/uml_diagram.svg").write_bytes(svg_output)
+        print("SVG diagram created successfully.")
+    else:
+        print("Failed to create SVG diagram.")
+
+
+if __name__ == "__main__":
+    generate_uml_diagram()
--- docs/package_graph.rst
+++ docs/package_graph.rst
@@ -1,58 +1,10 @@
 Package Graph
 =============
 
-.. uml::
-
-    @startuml
-    skinparam classAttributeIconSize 0
-    hide empty members
-
-    package "sam_ml" {
-
-        package "sam_ml.data" {
-            abstract class Data
-            package "sam_ml.data.preprocessing" {
-                circle " "
-            }
-            package "sam_ml.data.regio" {
-                circle "     "
-            }
-        }
-
-        package "sam_ml.models" {
-            abstract class Model
-            abstract class Classifier
-            abstract class Regressor
-            abstract class BasePipeline
-            protocol create_pipeline
-            class DynamicPipeline
-            package "sam_ml.models.classifier" {
-                circle "  "
-            }
-            package "sam_ml.models.regressor" {
-                circle "   "
-            }
-
-            abstract class AutoML
-            package "sam_ml.models.automl" {
-                circle "    "
-            }
-        }
-    }
-
-    AutoML <|-- "    "
-    Data <|-- " "
-    Model <|-- Classifier
-    Model <|-- Regressor
-    Model <|-- BasePipeline
-    Classifier <|-- "  "
-    Classifier <|-- DynamicPipeline
-    Regressor <|-- DynamicPipeline
-    Regressor <|-- "   "
-    DynamicPipeline <-- create_pipeline
-    BasePipeline <|-- DynamicPipeline
-
-    @enduml
+.. image:: _images/uml_diagram.svg
+   :alt: package uml diagram
+   :width: 600px
+   :align: center
 
 .. note::
 
